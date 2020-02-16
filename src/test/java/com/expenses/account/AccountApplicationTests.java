a80ae92ef4047d6c8b9d684ec2da4c3cb7730065
Felipe Mendes <felipemendes1994@gmail.com>
2020-02-16 20:36:36 -0300

    Criado classes basicas para configuracao e execucao dos testes automatizados

---
+++ src/test/java/com/expenses/account/AccountApplicationTests.java
@@ -0,0 +1,12 @@
+package com.expenses.account;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class AccountApplicationTests {
+
+}
