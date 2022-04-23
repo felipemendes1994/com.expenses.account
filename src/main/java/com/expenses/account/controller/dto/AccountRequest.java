2fb35616c27b2f46910825a03c961faf8beb08cb
Felipe Mendes <felipemendes1994@gmail.com>
2022-04-22 21:20:42 -0300

    feat: Refatora classes de request e response adotando lombok

--- src/main/java/com/expenses/account/controller/dto/AccountRequest.java
+++ src/main/java/com/expenses/account/controller/dto/AccountRequest.java
@@ -1,57 +1,20 @@
-package com.expenses.account.rest.dao;
+package com.expenses.account.controller.dto;
 
 import com.expenses.account.entity.Account;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.Data;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class AccountRequest {
 	private String accountType;
 	private String agencyNumber;
 	private String accountNumber;
 	private String bankNumber;
 	private String bankName;
-	
-	public String getAccountType() {
-		return accountType;
-	}
-	
-	public void setAccountType(String accountType) {
-		this.accountType = accountType;
-	}
-	
-	public String getAgencyNumber() {
-		return agencyNumber;
-	}
-	
-	public void setAgencyNumber(String agencyNumber) {
-		this.agencyNumber = agencyNumber;
-	}
-	
-	public String getAccountNumber() {
-		return accountNumber;
-	}
-	
-	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-	
-	public String getBankNumber() {
-		return bankNumber;
-	}
-	
-	public void setBankNumber(String bankNumber) {
-		this.bankNumber = bankNumber;
-	}
-
-	public String getBankName() {
-		return bankName;
-	}
-
-	public void setBankName(String bankName) {
-		this.bankName = bankName;
-	}
 	
 	public Account toAccount(){
 		Account account = new Account();
--- src/main/java/com/expenses/account/controller/dto/AccountResponse.java
+++ src/main/java/com/expenses/account/controller/dto/AccountResponse.java
@@ -1,4 +1,4 @@
-package com.expenses.account.rest.dao;
+package com.expenses.account.controller.dto;
 
 import java.math.BigDecimal;
 
@@ -7,8 +7,11 @@ import com.expenses.account.entity.AccountType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.Data;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class AccountResponse {
 	
 	private String bankNumber;
@@ -33,77 +36,5 @@ public class AccountResponse {
 		this.agencyNumber = account.getAgencyNumber();
 		this.accountNumber = account.getAccountNumber();
 		this.bankName = account.getBankName();
-	}
-
-	public String getAccountName() {
-		return accountName;
-	}
-
-	public void setAccountName(String accountName) {
-		this.accountName = accountName;
-	}
-
-	public String getAccountNick() {
-		return accountNick;
-	}
-
-	public void setAccountNick(String accountNick) {
-		this.accountNick = accountNick;
-	}
-
-	public AccountType getAccountType() {
-		return accountType;
-	}
-
-	public void setAccountType(AccountType accountType) {
-		this.accountType = accountType;
-	}
-
-	public String getBankNumber() {
-		return bankNumber;
-	}
-
-	public void setBankNumber(String bankNumber) {
-		this.bankNumber = bankNumber;
-	}
-
-	public BigDecimal getInitialBalance() {
-		return initialBalance;
-	}
-
-	public void setInitialBalance(BigDecimal initialBalance) {
-		this.initialBalance = initialBalance;
-	}
-
-	public BigDecimal getBalance() {
-		return balance;
-	}
-
-	public void setBalance(BigDecimal balance) {
-		this.balance = balance;
-	}
-
-	public String getAgencyNumber() {
-		return agencyNumber;
-	}
-
-	public void setAgencyNumber(String agencyNumber) {
-		this.agencyNumber = agencyNumber;
-	}
-
-	public String getAccountNumber() {
-		return accountNumber;
-	}
-
-	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-
-	public String getBankName() {
-		return bankName;
-	}
-
-	public void setBankName(String bankName) {
-		this.bankName = bankName;
 	}
 }
